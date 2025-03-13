5f33a84a048cbc61918d2981447aefa2c9519b3e
SajjadPourali <sajjad@pourali.com>
2025-03-13 13:26:59 -0400

    Add rule

--- aegis/src/main.rs
+++ aegis/src/main.rs
@@ -1,4 +1,37 @@
+use std::collections::HashMap;
+
+use aya::maps::HashMap as AyaHashMap;
+use rule::Rule;
+
+mod rule;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Rules {
+    rules: HashMap<String, Rule>,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let r = rule::Rule {
+        action: rule::Action::Forward,
+        host: rule::Host::Any,
+        port: rule::Num::Range(1, 10),
+        uid: rule::Num::Multi(vec![1, 2, 3, 4]),
+        pid: rule::Num::Any,
+    };
+    let r2 = rule::Rule {
+        action: rule::Action::Forward,
+        host: rule::Host::Domain("sss".to_string()),
+        port: rule::Num::Range(1, 10),
+        uid: rule::Num::Multi(vec![1, 2, 3, 4]),
+        pid: rule::Num::Any,
+    };
+    let mut h = HashMap::default();
+    h.insert("r1".to_string(), r);
+    h.insert("r2".to_string(), r2);
+    let rules = Rules { rules: h };
+    let o = toml::to_string(&rules).unwrap();
+    println!("{}", o);
     let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
         env!("OUT_DIR"),
         "/ebpf"
---
+++ aegis/src/rule/mod.rs
@@ -0,0 +1,120 @@
+use serde::{Deserialize, Serialize};
+use std::{net::IpAddr, num::IntErrorKind};
+
+fn default<T: Default + PartialEq>(t: &T) -> bool {
+    *t == Default::default()
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Action {
+    Allow,
+    Deny,
+    Forward,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Rule {
+    pub action: Action,
+    #[serde(flatten)]
+    #[serde(skip_serializing_if = "default")]
+    pub host: Host,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "default")]
+    pub port: Num,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "default")]
+    pub uid: Num,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "default")]
+    pub pid: Num,
+}
+
+#[derive(Debug, Default, PartialEq)]
+pub enum Num {
+    Singular(u32),
+    Range(u32, u32),
+    Multi(Vec<u32>),
+    #[default]
+    Any,
+}
+
+impl<'de> Deserialize<'de> for Num {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?
+            .chars()
+            .filter(|f| f.is_whitespace())
+            .collect::<String>();
+
+        if s.is_empty() {
+            return Ok(Num::Any);
+        }
+        if let Ok(n) = s.parse::<u32>() {
+            return Ok(Num::Singular(n));
+        }
+        if let Some((start, end)) = s.split_once('-').and_then(|(s, e)| {
+            s.parse::<u32>()
+                .map_err(|e| {
+                    if *e.kind() == IntErrorKind::Empty {
+                        Some(0)
+                    } else {
+                        None
+                    }
+                })
+                .ok()
+                .and_then(|s| {
+                    e.parse::<u32>()
+                        .map_err(|e| {
+                            if *e.kind() == IntErrorKind::Empty {
+                                Some(0)
+                            } else {
+                                None
+                            }
+                        })
+                        .ok()
+                        .map(|e| (s, e))
+                })
+        }) {
+            return Ok(Num::Range(start, end));
+        }
+
+        if s.contains(',') {
+            let nums: Result<Vec<u32>, _> = s.split(',').map(|x| x.parse::<u32>()).collect();
+            if let Ok(values) = nums {
+                return Ok(Num::Multi(values));
+            }
+        }
+
+        Err(serde::de::Error::custom("Invalid Format"))
+    }
+}
+
+impl Serialize for Num {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match self {
+            Num::Any => serializer.serialize_str(""),
+            Num::Singular(n) => serializer.serialize_str(&n.to_string()),
+            Num::Range(start, end) => serializer.serialize_str(&format!("{}-{}", start, end)),
+            Num::Multi(nums) => {
+                let nums_str = nums
+                    .iter()
+                    .map(|n| n.to_string())
+                    .collect::<Vec<_>>()
+                    .join(",");
+                serializer.serialize_str(&nums_str)
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
+pub enum Host {
+    Ip(IpAddr, u8),
+    Domain(String),
+    #[default]
+    Any,
+}
